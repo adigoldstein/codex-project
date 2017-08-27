cd020cb10581c49ad1e19c34afafb8ecb7b8665f
Adi <adij82@gmail.com>
2017-08-27 13:56:06 +0300

    first commit

---
+++ app/app.js
@@ -0,0 +1,9 @@
+import 'normalize.css/normalize.css';
+import 'font-awesome/css/font-awesome.css';
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+import './assets/styles/main.scss';
+import Root from './components/root/Root';
+
+ReactDOM.render(<Root/>, document.querySelector('#root'));
---
+++ app/components/job/Job.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import './job.css'
+
+export default class Job extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div className="job">
+        <h4 className="job-title">{this.props.job.title}</h4>
+        <div className="job-desc">{this.props.job.body}</div>
+        <div className="job-footer">
+          <div className="footer-left">
+            <img className="star-img" src="../../assets/images/star.png" alt=""/>
+            <span className="rank">{this.props.job.rank}</span>
+          </div>
+          <div data-network="sharethis" className="st-custom-button"><img className="share-img" src="../../assets/images/share.png" alt=""/></div>
+        </div>
+      </div>
+    )
+  }
+}
